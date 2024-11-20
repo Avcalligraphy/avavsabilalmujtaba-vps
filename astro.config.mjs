79b2792c2261ce99de3cbdfd880d942d9373d581
Avcalligraphy <avavsabilalmujtaba@gmail.com>
2024-11-21 06:30:24 +0700

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,9 +10,9 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://avavsabilalmujtaba.my.id",
-  // experimental: {
-  //   redirects: true,
-  // },
+  experimental: {
+    redirects: true,
+  },
   integrations: [
     tailwind(),
     mdx({
